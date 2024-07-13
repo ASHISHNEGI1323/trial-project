6e0924f97f341bbfd40bee2ed1381977a8991e28
ASHISHNEGI1323 <negi16111@gmail.com>
2024-07-13 10:45:35 +0530

    Weatherfiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-import Weather from "./Weather"
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
-  
-
   return (
     <>
       <Weather />
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
